40e059012d1c287bed415ea6057dfb8f08f8e166
nuronemli <nuronemli@gmail.com>
2022-07-30 14:07:28 +0100

    new child class toyota

--- src/day38_Inheritance/Bmw.java
+++ src/day38_Inheritance/Bmw.java
@@ -1,18 +1,20 @@
 package day38_Inheritance;
 
-public class Bmw extends Car{
+public class Bmw extends Car {
 
 
-
+    //remove brand parameter, add bmw as data
     public Bmw(String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
     }
 
-    public void breaksDown(){
-        System.out.println(brand+ " breaks down ");
+    public void breaksDown() {
+        System.out.println(brand + " breaks down ");
     }
 
-
+    public void racing() {
+        System.out.println(model + " is great for racing");
+    }
 
 
 }
